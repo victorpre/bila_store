6a2a4501509d8ec2697a0185495c348dd09c3eb2
Victor Presumido <victor.presumido@gmail.com>
2018-03-21 19:55:15 -0300

    Fix syntax on prod config file

--- config/prod.exs
+++ config/prod.exs
@@ -25,7 +25,7 @@ config :logger, level: :info
 config :bila_store, BilaStore.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE" || "10")),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE" || "10"))
 
 # ## SSL Support
 #
