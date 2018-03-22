8ed291d8ab4c8009a91eca631c706fd42c5ff70e
Victor Presumido <victor.presumido@gmail.com>
2018-03-22 01:10:05 -0300

    🔒 Make it better

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,6 @@ use Mix.Config
 config :bila_store, BilaStoreWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "http", host: "www.store.victorpre.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -27,7 +26,6 @@ config :bila_store, BilaStore.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE" || "10")),
-  ssl: true
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
